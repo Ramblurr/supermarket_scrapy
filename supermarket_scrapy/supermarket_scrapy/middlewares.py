5f3067adf3c067e092f7925c73ee3fc2a4d24bd7
Casey Link <me@caseylink.com>
2019-08-07 11:45:27 +0200

    Fix chrome usage in docker

--- supermarket_scrapy/supermarket_scrapy/middlewares.py
+++ supermarket_scrapy/supermarket_scrapy/middlewares.py
@@ -96,6 +96,8 @@ class UseChrome:
         # options.binary_location = '/usr/bin/chromium'
         options.add_argument("headless")
         options.add_argument("disable-gpu")
+        options.add_argument("disable-dev-shm-usage")
+        options.add_argument("no-sandbox")
         options.add_argument("window-size=1200x600")
         browser = webdriver.Chrome(chrome_options=options)
         self.browsers[0] = browser
